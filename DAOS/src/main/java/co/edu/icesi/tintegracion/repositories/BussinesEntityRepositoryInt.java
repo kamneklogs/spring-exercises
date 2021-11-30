2c76894935c3117347a538cb55335bd8302175d0
Camilo Cordoba <kamneklogs@gmail.com>
2021-11-30 17:30:24 -0500

    cleaned code

--- DAOS/src/main/java/co/edu/icesi/tintegracion/repositories/BussinesEntityRepositoryInt.java
+++
@@ -1,9 +0,0 @@
-package co.edu.icesi.tintegracion.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import co.edu.icesi.tintegracion.model.person.Businessentity;
-
-public interface BussinesEntityRepositoryInt extends CrudRepository<Businessentity, Integer>{
-    
-}
